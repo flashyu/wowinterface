645443fa23a704750a02c70b1fb090785aed9140
yupeng <flashyupeng@gmail.com>
2019-11-18 01:44:17 +0800

    u

--- WTF/Account/596395155#1/沙尔图拉/Actionscript/SavedVariables/TradeLog.lua
+++ WTF/Account/596395155#1/沙尔图拉/Actionscript/SavedVariables/TradeLog.lua
@@ -6,16 +6,16 @@ TradeLog_TradesHistory = {
 		["id"] = 1,
 		["playerItems"] = {
 			{
-				["numItems"] = 20,
-				["name"] = "魔法甜面包",
-				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
 				["texture"] = 133989,
+				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
+				["name"] = "魔法甜面包",
+				["numItems"] = 20,
 			}, -- [1]
 			{
-				["numItems"] = 20,
-				["name"] = "魔法甜面包",
-				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
 				["texture"] = 133989,
+				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
+				["name"] = "魔法甜面包",
+				["numItems"] = 20,
 			}, -- [2]
 		},
 		["targetItems"] = {
@@ -56,10 +56,10 @@ TradeLog_TradesHistory = {
 		["id"] = 3,
 		["playerItems"] = {
 			{
-				["texture"] = 132805,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["name"] = "魔法晶水",
 				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
 			}, -- [1]
 		},
 		["targetItems"] = {
@@ -81,10 +81,10 @@ TradeLog_TradesHistory = {
 		["id"] = 4,
 		["playerItems"] = {
 			{
-				["texture"] = 134122,
-				["itemLink"] = "|cff1eff00|Hitem:12811::::::::60:::::::|h[正义宝珠]|h|r",
-				["name"] = "正义宝珠",
 				["numItems"] = 2,
+				["name"] = "正义宝珠",
+				["itemLink"] = "|cff1eff00|Hitem:12811::::::::60:::::::|h[正义宝珠]|h|r",
+				["texture"] = 134122,
 			}, -- [1]
 		},
 		["targetItems"] = {
@@ -106,10 +106,10 @@ TradeLog_TradesHistory = {
 		["id"] = 5,
 		["playerItems"] = {
 			{
-				["texture"] = 132805,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["name"] = "魔法晶水",
 				["numItems"] = 19,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
 			}, -- [1]
 		},
 		["targetItems"] = {
@@ -131,16 +131,16 @@ TradeLog_TradesHistory = {
 		["id"] = 6,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [2]
 		},
 		["targetItems"] = {
@@ -153,8 +153,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "Javascript",
 		["when"] = "2019-11-16 22:41:36",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [6]
 	{
 		["targetMoney"] = 0,
@@ -167,22 +167,22 @@ TradeLog_TradesHistory = {
 			{
 				["name"] = "特效法力药水",
 				["numItems"] = 5,
-				["itemLink"] = "|cffffffff|Hitem:13444::::::::60:::::::|h[特效法力药水]|h|r",
 				["isUsable"] = true,
+				["itemLink"] = "|cffffffff|Hitem:13444::::::::60:::::::|h[特效法力药水]|h|r",
 				["texture"] = 134856,
 			}, -- [1]
 			{
 				["name"] = "特效法力药水",
 				["numItems"] = 5,
-				["itemLink"] = "|cffffffff|Hitem:13444::::::::60:::::::|h[特效法力药水]|h|r",
 				["isUsable"] = true,
+				["itemLink"] = "|cffffffff|Hitem:13444::::::::60:::::::|h[特效法力药水]|h|r",
 				["texture"] = 134856,
 			}, -- [2]
 		},
 		["player"] = "Actionscript",
+		["where"] = "通灵学院",
 		["who"] = "Javascript",
 		["when"] = "2019-11-16 22:43:06",
-		["where"] = "通灵学院",
 		["result"] = "cancelled",
 	}, -- [7]
 	{
@@ -191,16 +191,16 @@ TradeLog_TradesHistory = {
 		["id"] = 8,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [2]
 		},
 		["targetItems"] = {
@@ -213,8 +213,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "西加加",
 		["when"] = "2019-11-16 22:44:41",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [8]
 	{
 		["targetMoney"] = 0,
@@ -222,16 +222,16 @@ TradeLog_TradesHistory = {
 		["id"] = 9,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 133989,
+				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
 				["name"] = "魔法甜面包",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
-				["texture"] = 133989,
 			}, -- [2]
 		},
 		["targetItems"] = {
@@ -244,8 +244,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "過往的懮伤",
 		["when"] = "2019-11-16 22:46:07",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [9]
 	{
 		["targetMoney"] = 0,
@@ -253,16 +253,16 @@ TradeLog_TradesHistory = {
 		["id"] = 10,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [2]
 		},
 		["targetItems"] = {
@@ -275,8 +275,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "西加加",
 		["when"] = "2019-11-16 23:04:22",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [10]
 	{
 		["targetMoney"] = 0,
@@ -284,16 +284,16 @@ TradeLog_TradesHistory = {
 		["id"] = 11,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [2]
 		},
 		["targetItems"] = {
@@ -310,8 +310,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "Javascript",
 		["when"] = "2019-11-16 23:12:13",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [11]
 	{
 		["targetMoney"] = 0,
@@ -319,16 +319,16 @@ TradeLog_TradesHistory = {
 		["id"] = 12,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 19,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [2]
 		},
 		["targetItems"] = {
@@ -341,8 +341,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "西加加",
 		["when"] = "2019-11-16 23:51:38",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [12]
 	{
 		["targetMoney"] = 0,
@@ -350,16 +350,16 @@ TradeLog_TradesHistory = {
 		["id"] = 13,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [2]
 		},
 		["reason"] = "交易失败，交易目标没有足够的物品栏空间。",
@@ -371,8 +371,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "Javascript",
 		["when"] = "2019-11-16 23:57:00",
-		["where"] = "通灵学院",
 		["result"] = "error",
+		["where"] = "通灵学院",
 	}, -- [13]
 	{
 		["targetMoney"] = 0,
@@ -380,22 +380,22 @@ TradeLog_TradesHistory = {
 		["id"] = 14,
 		["playerItems"] = {
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [1]
 			{
+				["texture"] = 133989,
+				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
 				["name"] = "魔法甜面包",
 				["numItems"] = 20,
-				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
-				["texture"] = 133989,
 			}, -- [2]
 			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
 				["name"] = "魔法晶水",
 				["numItems"] = 19,
-				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
-				["texture"] = 132805,
 			}, -- [3]
 		},
 		["targetItems"] = {
@@ -410,8 +410,8 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "逞强而已丶",
 		["when"] = "2019-11-17 01:40:22",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [14]
 	{
 		["targetMoney"] = 0,
@@ -429,9 +429,421 @@ TradeLog_TradesHistory = {
 		},
 		["who"] = "骚骚珲",
 		["when"] = "2019-11-17 02:37:06",
-		["where"] = "通灵学院",
 		["result"] = "complete",
+		["where"] = "通灵学院",
 	}, -- [15]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 16,
+		["playerItems"] = {
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "队长缺德嘛",
+		["when"] = "2019-11-17 13:53:17",
+		["result"] = "complete",
+		["where"] = "斯坦索姆",
+	}, -- [16]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 17,
+		["playerItems"] = {
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [2]
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [3]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "Javascript",
+		["when"] = "2019-11-17 15:13:26",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [17]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 18,
+		["playerItems"] = {
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [2]
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [3]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "西加加",
+		["when"] = "2019-11-17 15:15:39",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [18]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 19,
+		["playerItems"] = {
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [2]
+			{
+				["numItems"] = 15,
+				["name"] = "魔法甜面包",
+				["itemLink"] = "|cffffffff|Hitem:8076::::::::60:::::::|h[魔法甜面包]|h|r",
+				["texture"] = 133989,
+			}, -- [3]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "咆哮长坂坡",
+		["when"] = "2019-11-17 15:36:42",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [19]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 20,
+		["playerItems"] = {
+			{
+				["numItems"] = 16,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "西加加",
+		["when"] = "2019-11-17 16:13:00",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [20]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 21,
+		["playerItems"] = {
+		},
+		["targetItems"] = {
+			{
+				["name"] = "雄鹰之天使护肩",
+				["numItems"] = 1,
+				["itemLink"] = "|cff1eff00|Hitem:10113::::::872:1656749184:60:::1::::|h[雄鹰之天使护肩]|h|r",
+				["isUsable"] = false,
+				["texture"] = 135039,
+			}, -- [1]
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "西加加",
+		["when"] = "2019-11-17 16:21:22",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [21]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 22,
+		["playerItems"] = {
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [2]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "Javascript",
+		["when"] = "2019-11-17 16:27:37",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [22]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 23,
+		["playerItems"] = {
+		},
+		["reason"] = "selfrunaway",
+		["targetItems"] = {
+			{
+				["name"] = "符文布",
+				["numItems"] = 20,
+				["itemLink"] = "|cffffffff|Hitem:14047::::::::60:::::::|h[符文布]|h|r",
+				["isUsable"] = true,
+				["texture"] = 132903,
+			}, -- [1]
+			{
+				["name"] = "符文布",
+				["numItems"] = 20,
+				["itemLink"] = "|cffffffff|Hitem:14047::::::::60:::::::|h[符文布]|h|r",
+				["isUsable"] = true,
+				["texture"] = 132903,
+			}, -- [2]
+			{
+				["name"] = "符文布",
+				["numItems"] = 20,
+				["itemLink"] = "|cffffffff|Hitem:14047::::::::60:::::::|h[符文布]|h|r",
+				["isUsable"] = true,
+				["texture"] = 132903,
+			}, -- [3]
+			{
+				["name"] = "符文布",
+				["numItems"] = 20,
+				["itemLink"] = "|cffffffff|Hitem:14047::::::::60:::::::|h[符文布]|h|r",
+				["isUsable"] = true,
+				["texture"] = 132903,
+			}, -- [4]
+			{
+				["name"] = "符文布",
+				["numItems"] = 20,
+				["itemLink"] = "|cffffffff|Hitem:14047::::::::60:::::::|h[符文布]|h|r",
+				["isUsable"] = true,
+				["texture"] = 132903,
+			}, -- [5]
+		},
+		["player"] = "Actionscript",
+		["where"] = "通灵学院",
+		["who"] = "西加加",
+		["when"] = "2019-11-17 16:44:27",
+		["result"] = "cancelled",
+	}, -- [23]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 24,
+		["playerItems"] = {
+		},
+		["reason"] = "other",
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["where"] = "通灵学院",
+		["who"] = "西加加",
+		["when"] = "2019-11-17 16:48:29",
+		["result"] = "cancelled",
+		["toofar"] = "no",
+	}, -- [24]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 25,
+		["playerItems"] = {
+			{
+				["texture"] = 132805,
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["name"] = "魔法晶水",
+				["numItems"] = 20,
+			}, -- [1]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "西加加",
+		["when"] = "2019-11-17 17:35:46",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [25]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 26,
+		["playerItems"] = {
+		},
+		["reason"] = "selfrunaway",
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["where"] = "通灵学院",
+		["who"] = "西加加",
+		["when"] = "2019-11-17 17:38:26",
+		["result"] = "cancelled",
+	}, -- [26]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 27,
+		["playerItems"] = {
+		},
+		["targetItems"] = {
+			{
+				["name"] = "图样：恶魔布袍",
+				["numItems"] = 1,
+				["isUsable"] = true,
+				["itemLink"] = "|cff1eff00|Hitem:14506::::::::60:::1::::|h[图样：恶魔布袍]|h|r",
+				["texture"] = 134942,
+			}, -- [1]
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "西加加",
+		["when"] = "2019-11-17 17:39:49",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [27]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 28,
+		["playerItems"] = {
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+			{
+				["numItems"] = 18,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [2]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "塔穆丶逐星",
+		["when"] = "2019-11-17 18:34:21",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [28]
+	{
+		["targetMoney"] = 0,
+		["playerMoney"] = 0,
+		["id"] = 29,
+		["playerItems"] = {
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [1]
+			{
+				["numItems"] = 20,
+				["name"] = "魔法晶水",
+				["itemLink"] = "|cffffffff|Hitem:8079::::::::60:::::::|h[魔法晶水]|h|r",
+				["texture"] = 132805,
+			}, -- [2]
+		},
+		["targetItems"] = {
+		},
+		["player"] = "Actionscript",
+		["events"] = {
+			"TRADE_SHOW", -- [1]
+			"TRADE_CLOSED", -- [2]
+			"TRADE_CLOSED", -- [3]
+		},
+		["who"] = "西加加",
+		["when"] = "2019-11-18 00:03:18",
+		["result"] = "complete",
+		["where"] = "通灵学院",
+	}, -- [29]
 }
 TRADE_LOG_BUTTON_POS = 138.96568260025
 TradeLog_Announce_Checked = true
